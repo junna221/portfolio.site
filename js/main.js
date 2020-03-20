553fccfc1b47562127cc4b9a5b8bfca7a830f03b
kaijunna <shell1016221@gmail.com>
2020-03-20 17:00:56 +0900

    no message

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-var mql = window.matchMedia('screen and (max-width: 599px)');
+var mql = window.matchMedia('screen and (max-width: 700px)');
   function checkBreakPoint(mql) {
     if (mql.matches) {
-      // スマホ向け（599px以下のとき）
+      // スマホ向け（700px以下のとき）
       $('.slider').not('.slick-initialized').slick({
         //スライドさせる
         slidesToShow: 1,
